1fc7f78a6930c5af6bd61b86e482a8d940a7cd82
o8c9lnu4 <apikovmaksim889@gmail.com>
2025-09-10 21:43:35 +0300

    Show only published posts; serve Vue SPA; axios baseURL env

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -147,6 +147,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blog/static/blog'),
+    os.path.join(BASE_DIR, 'frontend/dist'),  # Vue сборка для WhiteNoise
 ]
 
 # WhiteNoise settings
